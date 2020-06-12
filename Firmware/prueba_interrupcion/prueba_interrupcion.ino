b755c6c25ee554726d6c961ede45ab3cca1511a7
Textiles Licratex C.A <62059909+licratex@users.noreply.github.com>
2020-06-12 12:13:56 -0400

    Version funcionando en escritorio
    Pendiente probar montada en maquina, funciondo calculo de rendiminnto, mide metraje y calcula gramaje, el peso y el ancho se ponen por potenciometro mientras llegan los equipos.

--- Firmware/prueba_interrupcion/prueba_interrupcion.ino
+++ Firmware/prueba_interrupcion/prueba_interrupcion.ino
@@ -16,7 +16,7 @@ const byte ledPin       = 13;   // pin de led solo para monitorear
 const byte interruptPin = 7;    // pin al que esta conectada la interrupcion
 const byte zeroPin      = 5;    // boton setear a cero la longiud y el peso
 const byte enviar       = 8;    // boton para enviar
-const byte pot_ancho    = 18;    // potenciometro para poner el ancho (esto se cambiará por 4 celdas de carga que llevarán el carrito)
+const byte pot_ancho    = 21;    // potenciometro para poner el ancho (esto se cambiará por 4 celdas de carga que llevarán el carrito)
 const byte pot_peso     = 19;    // potenciometro para poner el ancho (esto se cambiará por 4 celdas de carga que llevarán el carrito)
 const byte boton_tela   = 20;    // Boton para selección de tela
 
@@ -31,7 +31,7 @@ int gramaje = 0;                // en gramos x m2
 int humedad = 0;                 // en %
 int rollo = 1;
 float rendimiento = 0;          // en metros / kg
-int tipo=1;                     // variable para que el usuario seleccione el tipo de tela
+int tipo = 1;                   // variable para que el usuario seleccione el tipo de tela
 
 void setup() {
   lcd.begin(20, 4);
@@ -53,27 +53,29 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ledPin, state);
-  interrupcion();             // lleva el conteo del encoder para calcular los metros de largo
-  zero();                     // boton de zero o tara, para calibrar a zero el peso al encender la máquina o si se descalibra.
+    digitalWrite(ledPin, state);
+    interrupcion();             // lleva el conteo del encoder para calcular los metros de largo
+    zero();                     // boton de zero o tara, para calibrar a zero el peso al encender la máquina o si se descalibra.
   tela();
-  wide();
-  weight();
-  pantalla();                 // cada 250 ms se refresca la pantalla del LCD con la info nueva
-  teclado();                  // envía por teclado la información, SOLO ENVÍA BAJO REQUEST
+    wide();
+    weight();
+    pantalla();                 // cada 250 ms se refresca la pantalla del LCD con la info nueva
+    teclado();                  // envía por teclado la información, SOLO ENVÍA BAJO REQUEST
 }
 
-void wide(){
-  ancho=analogRead(pot_ancho); // lee el valor del potenciometrom
-  ancho=map(ancho, 1, 1023, 1.19, 2.20);
+void wide() {
+  ancho = analogRead(pot_ancho); // lee el valor del potenciometrom
+  ancho = map(ancho, 1, 1023, 2000, 1190);
+  ancho = ancho / 1000;
 }
 
-void weight(){
-  peso=analogRead(pot_peso); // lee el valor del potenciometro
-  peso=map(peso  , 1, 1023, 5, 30);
+void weight() {
+  peso = analogRead(pot_peso); // lee el valor del potenciometro
+  peso = map(peso  , 1, 1023, 2500, 1500);
+  peso = peso / 100;
 }
 
-void tela(){
+void tela() {
   // Al presionar el boton de tipo de tela se cambia en la pantalla y en el valor enviado el tipo de tela
   // 1 Jersey
   // 2 PIQUE
@@ -84,22 +86,26 @@ void tela(){
   // 7 Microfibra
   // 8 Cotton Lycra
   // 9 PolyCottonLycra
-  if(!digitalRead(boton_tela)){
-    tipo++;
-    if(tipo>9){
-      tipo=1;
+ 
+    if(analogRead(boton_tela)<500){
+      tipo++;
+      if(tipo>9){
+        tipo=1;
+      }
+    while(analogRead(boton_tela)<500){
+      delay(300);
     }
-  while(!digitalRead(boton_tela)){
-    delay(300);
-  }
-  }
+    }
 }
 
 void pantalla() {
 
   if (millis() % 250 == 0) {
     gramaje = (peso * 1000) / (ancho * largo);
-    rendimiento = largo / peso;  
+    if (gramaje < 100 || gramaje > 500) {
+      gramaje = 0;
+    }
+    rendimiento = largo / peso;
     lcd.setCursor(0, 0);
     lcd.print("MTS :");
     lcd.print(largo);
@@ -122,35 +128,35 @@ void pantalla() {
     lcd.setCursor(11, 2);
     lcd.print("HUME:");
     lcd.print(humedad);
-    lcd.setCursor(11, 3);
-    switch(tipo){
+    lcd.setCursor(0, 3);
+    switch (tipo) {
       case 1:
-      lcd.print("Jersey");
-      break;
+        lcd.print("Jersey    ");
+        break;
       case 2:
-      lcd.print("Pique");
-      break;
+        lcd.print("Pique     ");
+        break;
       case 3:
-      lcd.print("Rib");
-      break;
+        lcd.print("Rib       ");
+        break;
       case 4:
-      lcd.print("Sabina");
-      break;
+        lcd.print("Sabina   ");
+        break;
       case 5:
-      lcd.print("MicrofLy");
-      break;
+        lcd.print("MicrofLyc");
+        break;
       case 6:
-      lcd.print("Interloc");
-      break;
+        lcd.print("Interlock");
+        break;
       case 7:
-      lcd.print("Microfib");
-      break;
+        lcd.print("Microfibr");
+        break;
       case 8:
-      lcd.print("AlgLycr");
-      break;
+        lcd.print("AlgLycra ");
+        break;
       case 9:
-      lcd.print("PolAlLy");
-      break;
+        lcd.print("PolyAlgLyc");
+        break;
     }
   }
 }
@@ -172,34 +178,34 @@ void teclado() {
     Keyboard.write(0xB3);  //        // TAB
     Keyboard.print(gramaje);  //        // TAB
     Keyboard.write(0xB3);  //        // TAB
-     switch(tipo){
+    switch (tipo) {
       case 1:
-      Keyboard.print("Jersey");
-      break;
+        Keyboard.print("Jersey");
+        break;
       case 2:
-      Keyboard.print("Pique");
-      break;
+        Keyboard.print("Pique");
+        break;
       case 3:
-      Keyboard.print("Rib");
-      break;
+        Keyboard.print("Rib");
+        break;
       case 4:
-      Keyboard.print("Sabina");
-      break;
+        Keyboard.print("Sabina");
+        break;
       case 5:
-      Keyboard.print("Microfibra Lycrada");
-      break;
+        Keyboard.print("Microfibra Lycrada");
+        break;
       case 6:
-      Keyboard.print("Interlock");
-      break;
+        Keyboard.print("Interlock");
+        break;
       case 7:
-      Keyboard.print("Microfibra");
-      break;
+        Keyboard.print("Microfibra");
+        break;
       case 8:
-      Keyboard.print("Algodon Lycra");
-      break;
+        Keyboard.print("Algodon Lycra");
+        break;
       case 9:
-      Keyboard.print("Polyester Algodon Lycra");
-      break;
+        Keyboard.print("Polyester Algodon Lycra");
+        break;
     }
     Keyboard.write(0xB3);  //        // TAB
     largo = 0;
@@ -217,16 +223,22 @@ void zero() { // funcion que lleva todas las variables a zero, sirve para calibr
     largo = 0;
     peso = 0;
     rollo++;
-    lcd.clear();
-    int c=0;        //variable para determinar si el boton de zero se dejo presionado mas de 5 segundos, con lo gual se pone en zero tambien la cantidad de rollos.
+    int c = 0;      //variable para determinar si el boton de zero se dejo presionado mas de 5 segundos, con lo gual se pone en zero tambien la cantidad de rollos.
     while (!digitalRead(zeroPin)) {
       c++;
       delay(100);
     }
-    if(c>50){
-      rollo=0;
+    if (c > 20) {
+      rollo = 0;
+      lcd.setCursor(11, 3);
+      lcd.print("ROLL:");
+      lcd.print("   ");
+      lcd.setCursor(11, 3);
+      lcd.print("ROLL:");
+      lcd.print(rollo);
     }
-    delay(1000);
+    lcd.clear();
+    delay(100);
   }
 }
 
